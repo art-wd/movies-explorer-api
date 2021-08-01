71d18cba2c90d7111e562b995e4e393c74a9b2c6
Artem Chagin <artem2020wd@yandex.ru>
2021-08-01 16:38:31 +0300

    feat: added data parser and connected all routes to app

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const cookieParser = require('cookie-parser');
 
 const { PORT = 3000 } = process.env;
 
+const router = require('./routes/index');
+
 const app = express();
+
 app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/beatfilmsdb', {
   useNewUrlParser: true,
@@ -15,6 +20,8 @@ mongoose.connect('mongodb://localhost:27017/beatfilmsdb', {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
+
+app.use(router);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
